9330ac9a140f4117e5469b252fa88e2ef58a0615
simone-danzeri <s.danzeri@icloud.com>
2024-05-29 18:43:51 +0200

    feat: add orders table (migration)

---
+++ database/migrations/2024_05_29_164316_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_client', 50);
+            $table->decimal('price_delivery', 8, 2);
+            $table->tinyint('number_order');
+            $table->string('payment_type', 25);
+            $table->boolean('payment_status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
